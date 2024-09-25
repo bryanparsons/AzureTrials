50d5d15518a33bde63fecd9c7f2175ea88d66ae9
bryan.parsons <bryan.parsons@bud.co.uk>
2024-09-25 19:10:44 +0100

    updated to work for Bud bulk deletes

--- cosmos-bulk-delete/DeleteAllRecords.ps1
+++ cosmos-bulk-delete/DeleteAllRecords.ps1
@@ -3,13 +3,13 @@ Clear-Host
 . $PSScriptRoot\common.ps1
 
 Write-Host "Getting Cosmos context"
-$CosmosContext=New-CosmosDbContext -Database $Global:CustomersManagementDatabase -ResourceGroupName $Global:CosmosResourceGroup  -Account $Global:CosmosAccountName
+$CosmosContext=New-CosmosDbContext -Database $Global:CosmosDatabase -ResourceGroupName $Global:CosmosResourceGroup  -Account $Global:CosmosAccountName
 Write-Host ("Got Cosmos context for Cosmos account: '{0}'  and database: '{1}'" -f $CosmosContext.Account, $CosmosContext.Database)
 
 
 function GetAllDocuments {
     #Remember to include Partition key path in field list
-    $allDocs = Get-CosmosDbDocument -CollectionId $Global:CustomersMasterContainer -QueryEnableCrossPartition $true -Query "SELECT c.id FROM c" -Context $CosmosContext
+    $allDocs = Get-CosmosDbDocument -CollectionId $Global:CosmosContainer -QueryEnableCrossPartition $true -Query "SELECT c.id, c.targetUserType FROM c WHERE c.deactivationDateTime < '2020-12-31T00:00:00'" -Context $CosmosContext
     if ($null -eq $allDocs)
     {
         return ,@()
@@ -21,10 +21,15 @@ function  DeleteDocuments {
     param ($documents)
     foreach ($doc in $documents) {
         #Remember to specify Partition key
-        Remove-CosmosDbDocument -Context $CosmosContext -CollectionId $Global:CustomersMasterContainer -Database $Global:CustomersManagementDatabase -Id $doc.id -PartitionKey $doc.id
+        Remove-CosmosDbDocument -Context $CosmosContext -CollectionId $Global:CosmosContainer -Database $Global:CosmosDatabase -Id $doc.id -PartitionKey $doc.targetUserType
     }
 }
 $documents=GetAllDocuments
-Write-Host ("Found {0} documents in the Container {1}" -f $documents.length,$Global:CustomersMasterContainer)
-DeleteDocuments -documents $documents
-Write-Host "Deletion complete"
+Write-Host ("`nFound {0} documents in the Container {1} within Account {2}." -f $documents.length,$Global:CosmosContainer,$CosmosContext.Account)
+
+$confirmation = Read-Host "`nAre you sure you want to permanently delete them? (enter y to proceed)"
+if ($confirmation -eq 'y') {
+    DeleteDocuments -documents $documents
+    Write-Host "Deletion complete"
+}
+
--- cosmos-bulk-delete/common.ps1
+++ cosmos-bulk-delete/common.ps1
@@ -3,9 +3,9 @@ $ErrorActionPreference="Stop"
 <#
 Declare global variables here
 #>
-$Global:CosmosResourceGroup="rg-demo-cosmos-serverless"
-$Global:CosmosAccountName="saudemocosmosserverless"
-$Global:CustomersManagementDatabase="CustomerManagement"
-$Global:CustomersMasterContainer="customers"
+$Global:CosmosResourceGroup="qa-plt-rg"
+$Global:CosmosAccountName="qa-bud-plt-cosmos-uks"
+$Global:CosmosDatabase="notifications"
+$Global:CosmosContainer="AlertDeactivationArchive"
 $Global:Location="uksouth"
 
