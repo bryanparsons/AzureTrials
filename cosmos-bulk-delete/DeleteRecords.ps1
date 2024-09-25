36c9074cb650c343fe48cbca77d26d48443b6ad9
bryan.parsons <bryan.parsons@bud.co.uk>
2024-09-25 19:40:25 +0100

    final tidy up

--- cosmos-bulk-delete/DeleteRecords.ps1
+++ cosmos-bulk-delete/DeleteRecords.ps1
@@ -9,7 +9,7 @@ Write-Host ("Got Cosmos context for Cosmos account: '{0}'  and database: '{1}'" 
 
 function GetAllDocuments {
     #Remember to include Partition key path in field list
-    $allDocs = Get-CosmosDbDocument -CollectionId $Global:CosmosContainer -QueryEnableCrossPartition $true -Query "SELECT c.id, c.targetUserType FROM c WHERE c.deactivationDateTime < '2020-12-31T00:00:00'" -Context $CosmosContext
+    $allDocs = Get-CosmosDbDocument -CollectionId $Global:CosmosContainer -QueryEnableCrossPartition $true -Query "SELECT c.id, c.targetUserType FROM c WHERE c.deactivationDateTime < '2019-12-31T00:00:00'" -Context $CosmosContext
     if ($null -eq $allDocs)
     {
         return ,@()
@@ -26,6 +26,8 @@ function  DeleteDocuments {
 }
 $documents=GetAllDocuments
 Write-Host ("`nFound {0} documents in the Container {1} within Account {2}." -f $documents.length,$Global:CosmosContainer,$CosmosContext.Account)
+# Check the account matches by using 'SELECT count(c.id) FROM c WHERE ******' in Cosmos, it should match the number returned here.
+# If the count of files is very high, in excess of 45,000, you may get an inaccurate count from this script as the query will time out.
 
 $confirmation = Read-Host "`nAre you sure you want to permanently delete them? (enter y to proceed)"
 if ($confirmation -eq 'y') {
